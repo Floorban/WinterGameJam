5d34f7b506e5bc131e6062465c9f06d532a0513b
isolight <122993708+isolight369@users.noreply.github.com>
2023-12-21 22:01:55 +0800

    Boss door and position fix

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -14,8 +14,9 @@ public class BossController : MonoBehaviour
  
     // Start is called before the first frame update
     void Start()
-    {;
+    {
         _timeBetweenAction = Random.Range(15f, 35f);
+        Debug.Log(_timeBetweenAction);
     }
 
     // Update is called once per frame
@@ -24,38 +25,38 @@ public class BossController : MonoBehaviour
         _timeBetweenAction -= Time.deltaTime;
         if (_timeBetweenAction <= 0 )
         {
+            Debug.Log("time hit 0");
             StartCoroutine(BossAction());
         }
     }
     
     [SerializeField] private GameObject _player;
     IEnumerator BossAction()
-    {
+    {       
+        Debug.Log("Coroutine");
         float _timeToWait = Random.Range(0.5f, 4.0f);
+        _timeBetweenAction = Random.Range(15f, 35f) + _timeToWait + 2f;
         _audioSource.PlayOneShot(_footSteps, 0.7F);
         yield return new WaitForSeconds(_timeToWait);
         OpenDoor();
-        transform.position += new Vector3(10, 0, 0);
-        _player.GetComponent<PlayerController>().CheckSleep();
-        yield return new WaitForSeconds(2f);
-        _player.GetComponent<PlayerController>().CheckSleep();
-        CloseDoor();
-        _timeBetweenAction = Random.Range(15f, 35f);
-
-
+        //_player.GetComponent<PlayerController>().CheckSleep();
+        yield return new WaitForSeconds(3f);
+        Debug.Log("wait");
+        //_player.GetComponent<PlayerController>().CheckSleep();
+        CloseDoor();      
     }
     #region DoorHandling
     [SerializeField] private GameObject _door;
     void OpenDoor()
     {
-        _door.transform.Rotate(90.0f, 0.0f, 0.0f);
-        transform.position += new Vector3(2, 0, 0);
+        _door.transform.Rotate(0.0f, -90.0f, 0.0f);
+        transform.position += new Vector3(3, 0, 0);
     }
 
     void CloseDoor()
     {
-        _door.transform.Rotate(0.0f, 0.0f, 0.0f);
-        transform.position += new Vector3(-2, 0, 0);
+        _door.transform.Rotate(0.0f, 90.0f, 0.0f);
+        transform.position += new Vector3(-3, 0, 0);
     }
     #endregion
 
