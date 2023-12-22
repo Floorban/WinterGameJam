b34f78c4a01c560531ab7ed7fa2eb66934faaddf
Floorban <15696082812@163.com>
2023-12-22 07:56:27 +0100

    mm

--- Assets/Scripts/PhoneState.cs
+++ Assets/Scripts/PhoneState.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
 
 public class PhoneState : MonoBehaviour
 {
@@ -7,12 +7,16 @@ public class PhoneState : MonoBehaviour
     public bool canPickup;
     [SerializeField]
     private float callTime, waitTime;
-
+    private Animator animator;
     [SerializeField] private AudioClip _ringSound;
     [SerializeField] private AudioSource _audioSource;
+
+    public PlayerController player;
+    public UnityEvent GiveStrike;
     void Start()
     {
         waitTime = Random.Range(2f, 10f);
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,6 +24,7 @@ public class PhoneState : MonoBehaviour
         waitTime -= Time.deltaTime;
         if (waitTime <= 0)
         {
+            waitTime = Random.Range(2f, 10f);
             PhoneRing();
         }
     }
@@ -27,13 +32,14 @@ public class PhoneState : MonoBehaviour
     {
         _audioSource.Play();
         canPickup = true;
+        animator.SetBool("hasCall", true);
 
         float waitForPickup = 4f;
         waitForPickup -= Time.deltaTime;
 
-        if (waitForPickup <= 0)
+        if (waitForPickup <= 0 && !player.isCalling)
         {
-            // get a strike
+            GiveStrike.Invoke();
         }
     }
     void OpenTextPanel()
